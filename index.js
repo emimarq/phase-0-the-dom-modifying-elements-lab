888caf77b87ad7dc3f147e108b699478ebe1a67a
Emilio <marqetm@gmail.com>
2022-06-01 22:07:02 -0500

    commit tree 6a6e2c6c2fea7703ad1c987e23c381e18b8e3c51

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const domNode = document.getElementById('main')
+domNode.remove()
+
+const newHeader = document.createElement('h1')
+newHeader.id = "victory"
+newHeader.innerHTML = "Emilio is the champion"
+document.body.append(newHeader)
